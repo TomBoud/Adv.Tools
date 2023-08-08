93ff03944e3f38360621cef02831eaf41b9e488e
TomBoud <tom4544@gmail.com>
2023-08-08 22:35:32 +0300

    Save Work in Progress

--- Adv.Tools.DataAccess/MySql/IMySqlDataAccess.cs
+++ Adv.Tools.DataAccess/MySql/IMySqlDataAccess.cs
@@ -7,24 +7,10 @@ using System.Threading.Tasks;
 
 namespace Adv.Tools.DataAccess.MySql
 {
-    public class IMySqlDataAccess
+    public interface IMySqlDataAccess
     {
+        Task<List<T>> LoadData<T, U>(string sqlQuery, U parameters);
+        Task SaveData<T>(string sqlQuery, T parameters);
 
-        public async Task<List<T>> LoadData<T, U>(string sqlQuery, U parameters)
-        {
-            using (IDbConnection connection = new MySqlConnection(_connectionString))
-            {
-                var rows = await connection.QueryAsync<T>(sqlQuery, parameters);
-                return rows.ToList();
-            }
-        }
-
-        public async Task SaveData<T>(string sqlQuery, T parameters)
-        {
-            using (IDbConnection connection = new MySqlConnection(_connectionString))
-            {
-                await connection.ExecuteAsync(sqlQuery, parameters);
-            }
-        }
     }
 }
