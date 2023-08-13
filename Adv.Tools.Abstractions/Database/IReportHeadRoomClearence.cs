e770bf1aaad1a326452548c6b198195d625fcba0
Tom <tomb@adeng.co.il>
2023-08-13 13:06:47 +0300

    Save Work in Progress

---
+++ Adv.Tools.Abstractions/Database/IReportHeadRoomClearence.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Adv.Tools.Abstractions.Database
+{
+    public interface IReportHeadRoomClearence
+    {
+        int Id { get; set; }
+        string ModelName { get; set; }
+        string ModelGuid { get; set; }
+        string Disicpline { get; set; }
+        string ObjectName { get; set; }
+        string ObjectFamily { get; set; }
+        string ObjectLevel { get; set; }
+        string ObjectId { get; set; }
+    }
+}
---
+++ Adv.Tools.DataAccess/MySql/Models/ReportInternalClash.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Adv.Tools.Abstractions.Database;
+
+namespace Adv.Tools.DataAccess.MySql.Models
+{
+    public class ReportInternalClash : IReportInternalClash
+    {
+        public int Id { get; set; }
+        public string ModelName { get; set; }
+        public string ModelGuid { get; set; }
+        public string Disicpline { get; set; }
+        public string SourceLevelName { get; set; }
+        public string SourceCategory { get; set; }
+        public string SourceElementName { get; set; }
+        public string SourceElementId { get; set; }
+        public string ClashCategory { get; set; }
+        public string ClashLevelName { get; set; }
+        public string ClashElementName { get; set; }
+        public string ClashElementId { get; set; }
+    }
+}
